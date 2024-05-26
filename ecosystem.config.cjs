be17e65b69b32fe61d75edeb8892e9e701f667a5
BriqueAuBob <briqueaubob@gmail.com>
2024-05-26 11:01:13 +0200

    :wrench: Update ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'univ-portfolio',
-      script: './build/bin/server.js',
+      script: './bin/server.js',
       instances: 'max',
       exec_mode: 'cluster',
       autorestart: true,
