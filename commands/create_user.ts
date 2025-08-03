2174494608911ac79b9419f8f74f839c908293ec
BriqueAuBob <briqueaubob@gmail.com>
2025-08-03 13:28:37 +0200

    Create user command

---
+++ commands/create_user.ts
@@ -0,0 +1,44 @@
+import User from '#models/user';
+import { inject } from '@adonisjs/core';
+import { BaseCommand, flags } from '@adonisjs/core/ace';
+import { Hash } from '@adonisjs/core/hash';
+import type { CommandOptions } from '@adonisjs/core/types/ace';
+
+export default class CreateUser extends BaseCommand {
+  static commandName = 'create:user';
+  static description = '';
+
+  static options: CommandOptions = {};
+
+  @flags.string({
+    description: 'Email of the user to create',
+    required: true,
+    alias: 'e',
+  })
+  declare email: string;
+
+  @flags.string({
+    description: 'Password for the user',
+    required: true,
+    alias: 'p',
+  })
+  declare password: string;
+
+  async run() {
+    this.logger.info('Creating user with the provided email and password');
+
+    try {
+      await User.updateOrCreate(
+        { email: this.email },
+        {
+          email: this.email,
+          password: this.password,
+        }
+      );
+      this.logger.info('User created successfully');
+    } catch (err) {
+      this.logger.error('Error creating user');
+      return;
+    }
+  }
+}
